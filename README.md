519a79f46c1a8472efe080b67bedbb5f9150b974
Swqpping <swqpping@gmail.com>
2021-03-12 04:46:16 +0100

    v2

--- README.md
+++
@@ -1,1 +0,0 @@
-# Swqppingg.github.io
--- js/demo.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#e1e6e5',
-    lineColor: '#e1e6e5'
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
